d2493f28d30396a9b15132e4572b5e71955a233c
Roy Mugera <roymugera@icloud.com>
2022-02-24 04:56:15 +0000

    seventh commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function menuOpen () {
+    document.getElementById("side-menu").style.width = '290px';
+}
+function menuClose () {
+    document.getElementById("side-menu").style.width = '0';
+};
+
+
+// When clicking on the side
+function sideView() {
+    document.getElementById("side-menu").style.width = '0';
+};
