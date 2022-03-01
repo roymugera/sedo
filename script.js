5641da51d6518c527253c2f33b431e1a4739112c
Roy Mugera <roymugera@icloud.com>
2022-03-01 17:37:31 +0000

    reponsive design

--- script.js
+++ script.js
@@ -1,9 +1,129 @@
 function menuOpen () {
     document.getElementById("side_menu").style.width = '290px';
-    document.getElementById("hamburger_menu").style.visibility = 'hidden';
 }
+
 function menuClose () {
     document.getElementById("side_menu").style.width = '0';
 };
 
 
+function arrowUp () {
+    document.getElementById('shadowBox').style.height = 'auto';
+    document.getElementById('shadowBox').style.visibility = 'visible';
+    document.getElementById('content0').style.visibility = 'visible';
+}
+
+function arrowDown () {
+    document.getElementById('shadowBox').style.height = '0';
+    document.getElementById('shadowBox').style.visibility = 'hidden';
+    document.getElementById('content0').style.visibility = 'hidden';
+}
+
+// 1 
+
+function arrowUp1 () {
+    document.getElementById('shadowBox1').style.height = 'auto';
+    document.getElementById('shadowBox1').style.visibility = 'visible';
+    document.getElementById('content1').style.visibility = 'visible';
+}
+
+function arrowDown1 () {
+    document.getElementById('shadowBox1').style.height = '0';
+    document.getElementById('shadowBox1').style.visibility = 'hidden';
+    document.getElementById('content1').style.visibility = 'hidden';
+}
+
+
+// 2
+
+function arrowUp2 () {
+    document.getElementById('shadowBox2').style.height = 'auto';
+    document.getElementById('shadowBox2').style.visibility = 'visible';
+    document.getElementById('content2').style.visibility = 'visible';
+}
+
+function arrowDown2 () {
+    document.getElementById('shadowBox2').style.height = '0';
+    document.getElementById('shadowBox2').style.visibility = 'hidden';
+    document.getElementById('content2').style.visibility = 'hidden';
+}
+
+
+
+// 3
+
+function arrowUp3 () {
+    document.getElementById('shadowBox3').style.height = 'auto';
+    document.getElementById('shadowBox3').style.visibility = 'visible';
+    document.getElementById('content3').style.visibility = 'visible';
+}
+
+function arrowDown3 () {
+    document.getElementById('shadowBox3').style.height = '0';
+    document.getElementById('shadowBox3').style.visibility = 'hidden';
+    document.getElementById('content3').style.visibility = 'hidden';
+}
+
+
+// 4
+
+function arrowUp4 () {
+    document.getElementById('shadowBox4').style.height =  'auto';
+    document.getElementById('shadowBox4').style.visibility = 'visible';
+    document.getElementById('content4').style.visibility = 'visible';
+}
+
+function arrowDown4 () {
+    document.getElementById('shadowBox4').style.height = '0';
+    document.getElementById('shadowBox4').style.visibility = 'hidden';
+    document.getElementById('content4').style.visibility = 'hidden';
+}
+
+
+// 5
+
+function arrowUp5 () {
+    document.getElementById('shadowBox5').style.height =  'auto';
+    document.getElementById('shadowBox5').style.visibility = 'visible';
+    document.getElementById('content5').style.visibility = 'visible';
+}
+
+function arrowDown5 () {
+    document.getElementById('shadowBox5').style.height = '0';
+    document.getElementById('shadowBox5').style.visibility = 'hidden';
+    document.getElementById('content5').style.visibility = 'hidden';
+}
+
+// 6
+
+function arrowUp6 () {
+    document.getElementById('shadowBox6').style.height =  'auto';
+    document.getElementById('shadowBox6').style.visibility = 'visible';
+    document.getElementById('content6').style.visibility = 'visible';
+}
+
+function arrowDown6 () {
+    document.getElementById('shadowBox6').style.height = '0';
+    document.getElementById('shadowBox6').style.visibility = 'hidden';
+    document.getElementById('content6').style.visibility = 'hidden';
+}
+
+// 7
+
+function arrowUp7 () {
+    document.getElementById('shadowBox7').style.height =  'auto';
+    document.getElementById('shadowBox7').style.visibility = 'visible';
+    document.getElementById('content7').style.visibility = 'visible';
+}
+
+function arrowDown7 () {
+    document.getElementById('shadowBox7').style.height = '0';
+    document.getElementById('shadowBox7').style.visibility = 'hidden';
+    document.getElementById('content7').style.visibility = 'hidden';
+}
+
+
+// body 
+function mainPage() {
+    document.body.style.transition = '0.2s';
+}
